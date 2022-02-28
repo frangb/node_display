6c2c329ec6ca93630faba08de8737a1b25abfb22
Fran Garcia <frangb@gmail.com>
2022-02-28 08:59:30 +0100

    changes

--- node_display.py
+++ node_display.py
@@ -1,13 +1,13 @@
+#!/usr/bin/env python3
 
 from board import SCL, SDA
 import busio
 from PIL import Image, ImageDraw, ImageFont
 import adafruit_ssd1306
-import requests
 import logging
 import time
 import datetime
-from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
+from bitcoinrpc.authproxy import AuthServiceProxy
 from socket import error as socket_error
 import sys
 import configparser
@@ -44,6 +44,7 @@ i2c = busio.I2C(SCL, SDA)
 disp = adafruit_ssd1306.SSD1306_I2C(DISPLAY_WIDTH, DISPLAY_HEIGHT, i2c)
 clear()
 
+# Blinks and show "NEW BLK" when a new block is found
 def blink(seconds):
     image = Image.new('1', (DISPLAY_WIDTH, DISPLAY_HEIGHT))
     draw = ImageDraw.Draw(image)
@@ -60,26 +61,19 @@ def blink(seconds):
     
 def draw(block, blocktime):
     image = Image.new('1', (DISPLAY_WIDTH, DISPLAY_HEIGHT))
-
     draw = ImageDraw.Draw(image)
-
-    #  Escribe 2 lineas texto
     draw.text((50, 1),   blocktime,  font=font_time, fill=255)
     draw.text((14, 12), block, font=font_block, fill=255)
-
     disp.image(image)
     disp.show()
     
 def draw_err(err_code):
     image = Image.new('1', (DISPLAY_WIDTH, DISPLAY_HEIGHT))
     draw = ImageDraw.Draw(image)
-
-    draw.text((14, 12), block, font=font_err, fill=255)
-
+    draw.text((14, 12), current_block, font=font_err, fill=255)
     disp.image(image)
     disp.show()
     
-# rpc_user and rpc_password are set in the bitcoin.conf file
 rpc_user = parser.get("config", "rpc_user")
 rpc_pass = parser.get("config", "rpc_pass")
 rpc_host = parser.get("config", "rpc_host") # if running locally then 127.0.0.1
@@ -91,24 +85,24 @@ except socket_error as e:
     logging.error("Cannot connect to node")
     sys.exit()
     
-prev_data = rpc_client.getblockchaininfo()['blocks'] - 1
-prev_hash = rpc_client.getblockhash(prev_data)
+prev_blk_height = rpc_client.getblockchaininfo()['blocks'] - 1
+prev_hash = rpc_client.getblockhash(prev_blk_height)
 prev_block = rpc_client.getblock(prev_hash)
 prev_time = prev_block['time']
 
 while True:
-    data = rpc_client.getblockchaininfo()['blocks']
-    hash = rpc_client.getblockhash(data)
-    if isinstance(data, int):
-        if data != prev_data:
+    current_blk_height = rpc_client.getblockchaininfo()['blocks']
+    current_blk_hash = rpc_client.getblockhash(current_blk_height)
+    if isinstance(current_blk_height, int):
+        if current_blk_height != prev_blk_height:
             blink(6)
-            prev_data = data
-            hash = rpc_client.getblockhash(data)
-            block = rpc_client.getblock(hash)
-            current_time = block['time']
+            prev_blk_height = current_blk_height
+            current_blk_hash = rpc_client.getblockhash(current_blk_height)
+            current_block = rpc_client.getblock(current_blk_hash)
+            current_time = current_block['time']
             elapsed = current_time - prev_time
             elapsed = datetime.timedelta(seconds=elapsed)
-            draw(str(data), str(elapsed))
+            draw(str(current_blk_height), str(elapsed))
             prev_time = current_time
     else:
         draw_err(ERROR_CODES["No response"])
